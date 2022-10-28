d5d777432818c4ca049315463f67c29a868cee51
volk19891919 <volk19891919@yandex.ru>
2022-10-28 11:35:34 +0400

    2 commit

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+//import slick from 'slick-carousel'
+//import owl from 'owl.carousel'
+//import magnificpopup from 'magnific-popup'
+// require('./other_script.js') // Require Other Script(s) from app/js folder Example
+//import { Swiper, Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation } from 'swiper'
+//Swiper.use([ Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation ])
+
+//import { gsap, Power2 } from 'gsap'
+//import MicroModal from 'micromodal'
+//import { Fancybox } from "@fancyapps/ui/src/Fancybox/Fancybox.js"
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+
+	// let lazyLoadInstance = new LazyLoad({
+	// 	// Your custom settings go here
+	// });
+
+})
